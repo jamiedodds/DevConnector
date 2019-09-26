2f73b8c8a8db3d07d197ca5b6ea4027e845adb9d
jamiedodds <jamiedodds13@gmail.com>
2019-09-26 18:29:07 +0100

    react user authentication

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
 // import axios from 'axios';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -16,8 +19,13 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('Successful Login');
+        login(email, password);
     };
+
+    // Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />;
+    }
 
     return (
         <Fragment>
@@ -62,4 +70,16 @@ const Login = () => {
     );
 };
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+    mapStateToProps,
+    { login }
+)(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 // import axios from 'axios';
 
-const Register = props => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -20,9 +22,9 @@ const Register = props => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            props.setAlert('Passwords do not match', 'danger');
+            setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('Successful Register');
+            register({ name, email, password });
             // got rid to use Redux
             // const newUser = {
             //     name,
@@ -44,6 +46,11 @@ const Register = props => {
         }
     };
 
+    // Redirect when logged in
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />;
+    }
+
     return (
         <Fragment>
             <h1 className='large text-primary'>Sign Up</h1>
@@ -58,7 +65,7 @@ const Register = props => {
                         name='name'
                         value={name}
                         onChange={e => onChange(e)}
-                        required
+                        // required
                     />
                 </div>
                 <div className='form-group'>
@@ -68,7 +75,7 @@ const Register = props => {
                         name='email'
                         value={email}
                         onChange={e => onChange(e)}
-                        required
+                        // required
                     />
                     <small className='form-text'>
                         This site uses Gravatar so if you want a profile image,
@@ -83,7 +90,7 @@ const Register = props => {
                         minLength='6'
                         value={password}
                         onChange={e => onChange(e)}
-                        required
+                        // required
                     />
                 </div>
                 <div className='form-group'>
@@ -94,7 +101,7 @@ const Register = props => {
                         minLength='6'
                         value={password2}
                         onChange={e => onChange(e)}
-                        required
+                        // required
                     />
                 </div>
                 <input
@@ -110,7 +117,17 @@ const Register = props => {
     );
 };
 
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-    null,
-    { setAlert }
+    mapStateToProps,
+    { setAlert, register }
 )(Register);
